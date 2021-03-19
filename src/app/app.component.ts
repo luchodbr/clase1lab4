51fa1a79b87638811b7d4be7e567e1df810c71d5
Luciano Gil <lgil2@seidor.es>
2021-03-18 21:15:30 -0300

    Primer commit. Modificaciones para deploy heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Clase1-Angular';
+  public edad1 : number ;
+  public edad2 : number;
+  public suma :number;
+  public promedio:number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  Calcular() {
+    this.suma = this.edad1 + this.edad2;
+    this.promedio = (this.edad2 + this.edad1) / 2 ;
+  }
+  LimpiarCuadro(){
+    this.edad1 = null;
+    this.edad2 = null;
+    this.suma = null;
+    this.promedio = null;
+   }
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BienvenidoComponent } from './Ejercicio2/bienvenido/bienvenido.component';
+import { LoginComponent } from './Ejercicio2/login/login.component';
+import { ErrorComponent } from './Ejercicio2/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
